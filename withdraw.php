e1ed3d775e800fc3f24d1e3c03ca45d7e8509845
xxx-kohe-xxx <kohe.dekita@gmail.comkohe.dekita@gmail.comgit config --global user.name xxx-kohe-xxxgit remote -vgit config --global user.email kohe.dekita@gmail.comkohe.dekita@gmail.com>
2019-04-06 07:15:43 +0900

    退会機能実装

--- withdraw.php
+++ withdraw.php
@@ -1,4 +1,57 @@
 <?php
+//共通変数・関数ファイルを読み込み
+require('function.php');
+
+debug('「「「「「「「「「「「「「「「「「「「「「「「「「「「「「「');
+debug('「　退会ページ');
+debug('「「「「「「「「「「「「「「「「「「「「「「「「「「「「「「');
+debugLogStart();
+
+// ログイン認証 (ログインしていないと見れないページなので)
+require('auth.php');
+
+// ========================================
+// 画面処理
+// ========================================
+// post送信されていた場合
+if(!empty($_POST)){
+	debug('POST送信があります。');
+	// 例外処理
+	try{
+		// DBに接続
+		$dbh = dbconnect();
+		// SQL文を作成
+		$sql1 = 'UPDATE users SET del_flg = 1 WHERE user_id = :us_id';
+		$sql2 = 'UPDATE lists SET del_flg = 1 WHERE user_id = :us_id';
+		$sql3 = 'UPDATE likes SET del_flg = 1 WHERE user_id = :us_id';
+		// データ流し込み
+		$data = array(':us_id' => $_SESSION['user_id']);
+		// クエリ実行
+		$stmt1 = queryPost($dbh, $sql1, $data);
+		$stmt2 = queryPost($dbh, $sql2, $data);
+		$stmt3 = queryPost($dbh, $sql3, $data);
+
+		// クエリ実行成功の場合
+		if($stmt1){
+			// セッション削除
+			session_destroy();
+			debug('セッション変数の中身:'.print_r($_SESSION,true));
+			debug('トップページへ遷移します。');
+			header("Location:index.php");
+		}else{
+			debug('クエリが失敗しました。');
+			$err_msg['common'] = MSG07;
+		}
+	} catch(Exception $e){
+		error_log('エラー発生:'.$e->getMessage());
+		$err_msg['common'] = MSG07;
+	}
+}
+debug('画面表示処理終了 <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<');
+
+?>
+
+<?php
 $siteTitle = '退会';
 require('head.php');
 ?>
@@ -25,10 +78,15 @@ require('head.php');
 		<!-- メイン -->
 		<section id="main">
 			<div class="form-container">
-				<form action="" class="form">
+				<form action="" method="post" class="form">
 					<h2 class="title">退会</h2>
+					<div class="area-msg">
+						<?php
+						if(!empty($err_msg['common'])) echo $err_msg['common'];
+						?>
+					</div>
 					<div class="btn-container">
-						<input type="submit" class="btn btn-mid" value="退会する">	
+						<input type="submit" class="btn btn-mid" value="退会する" name="submit">	
 					</div>
 				</form>
 			</div>
