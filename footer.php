1416a333697471d6c36e9839036713c0c647b0e0
xxx-kohe-xxx <kohe.dekita@gmail.com>
2019-04-12 07:48:39 +0900

    jsShowMsgのスクリプトを追加

--- footer.php
+++ footer.php
@@ -1,5 +1,18 @@
 <footer>
-		Copyright
-		<a href="">SHARE-LIST</a>
-		. All Rights Reserved.
+	Copyright
+	<a href="">SHARE-LIST</a>
+	. All Rights Reserved.
 </footer>
+
+<script src="js/vendor/jquery-2.2.2.min.js"></script>
+<script>
+	$(function(){
+		var $jsShowMsg = $('#js-show-msg');
+		var msg = $jsShowMsg.text();
+		if(msg.replace(/^[\s ]+|[\s ]+$/g, "").length){
+			$jsShowMsg.slideToggle('slow');
+			setTimeout(function(){ $jsShowMsg.slideToggle('slow'); },5000);
+		}
+		
+	});
+</script>
