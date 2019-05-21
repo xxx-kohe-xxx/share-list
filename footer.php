9be753321c82653858b9ea7f21706170f19f08cf
xxx-kohe-xxx <kohe.dekita@gmail.com>
2019-05-22 06:01:49 +0900

    プロフィール画像登録機能

--- footer.php
+++ footer.php
@@ -7,12 +7,40 @@
 <script src="js/vendor/jquery-2.2.2.min.js"></script>
 <script>
 	$(function(){
+		// メッセージ表示
 		var $jsShowMsg = $('#js-show-msg');
 		var msg = $jsShowMsg.text();
 		if(msg.replace(/^[\s ]+|[\s ]+$/g, "").length){
 			$jsShowMsg.slideToggle('slow');
 			setTimeout(function(){ $jsShowMsg.slideToggle('slow'); },5000);
 		}
+
+		// 画像ライブプレビュー
+		var $dropArea = $('.area-drop');
+		var $fileInput = $('.input-file');
+		$dropArea.on('dragover', function(e){
+			e.stopPropagation();
+			e.preventDefault();
+			$(this).css('border','3px #CCC dashed');
+		});
+		$dropArea.on('dragleave', function(e){
+			e.stopPropagation();
+			e.preventDefault();
+			$(this).css('border','none');
+		});
+		$fileInput.on('change', function(e){
+			$dropArea.css('border', 'none');
+			var file = this.files[0],
+					$img = $(this).siblings('.prev-img'),
+					fileReader = new FileReader();
+
+			fileReader.onload = function(event){
+				$img.attr('src', event.target.result).show();
+			};
+
+			fileReader.readAsDataURL(file);
+
+		});
 		
 		// お気に入り登録・削除
 		var $like,
